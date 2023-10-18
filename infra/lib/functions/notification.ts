667c07e8914e4ff9588f6101023c05fa080fa41d
haandol <ldg55d@gmail.com>
2023-10-18 23:13:28 +0900

    use axio instead of got

--- infra/lib/functions/notification.ts
+++ infra/lib/functions/notification.ts
@@ -1,18 +1,27 @@
-import * as got from 'got';
+import axios from 'axios';
 import { SNSEvent } from 'aws-lambda';
 
 const HookUrl = process.env.HOOK_URL!;
 
 export const handler = async (event: SNSEvent) => {
   for (const record of event.Records) {
-    const message = JSON.parse(record.Sns.Message);
+    const message = record.Sns.Message;
+    console.log(message);
+
     if (HookUrl.length > 0) {
-      const resp = await got.post(HookUrl, {
-        json: message,
-      });
+      const resp = await axios.post(
+        HookUrl,
+        {
+          Content: record.Sns.Message,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
       console.log(resp);
     }
-    console.log(message);
   }
 
   return 'ok';
